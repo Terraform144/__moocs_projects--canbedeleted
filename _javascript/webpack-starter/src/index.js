915e51e07bceffcafa8fdf98b84e81e22b9ed2fb
The armors'll save us all <jl_beuzard@hotmail.com>
2022-11-05 00:12:52 +0100

    ! mod
    webpack.config.js bundle output

--- _javascript/webpack-starter/src/index.js
+++ _javascript/webpack-starter/src/index.js
@@ -1,4 +1,5 @@
 import {v4 as uuidv4 } from 'uuid';
 import generateJoke from "./generateJoke";
 
-console.log(generateJoke());console.log(uuidv4());
+console.log(generateJoke());
+console.log(uuidv4());
