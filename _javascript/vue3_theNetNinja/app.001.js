7957e558eac1e0fc39338b80e4226a9183b7dcd5
The armors'll save us all <jl_beuzard@hotmail.com>
2022-11-20 00:08:10 +0100

    !ninja test vue 1

---
+++ _javascript/vue3_theNetNinja/app.001.js
@@ -0,0 +1,24 @@
+// https://www.youtube.com/watch?v=F7PLPJqVotk&list=PL4cUxeGkcC9hYYGbV60Vq3IXYNfDk8At1&index=2
+
+/*
+const app = Vue.createApp({
+    // data, function
+    template : '<h2>I am the template</h2>'
+}); */
+
+const app = Vue.createApp({
+    data() {
+        return {
+            title : 'The empire',
+            author : 'the two',
+            age : 45
+        }
+    },
+    methods: {
+        changeTitle() {
+            this.title = "new title from methods"
+        }
+    }
+});
+
+app.mount('#app');
