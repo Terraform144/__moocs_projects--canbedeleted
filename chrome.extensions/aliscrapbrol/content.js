6730cf1c41187ae26c0bb05ff1e935a6ac629567
zulad <info@terraform144.eu>
2023-03-04 01:54:11 +0100

    add ext

---
+++ chrome.extensions/aliscrapbrol/content.js
@@ -0,0 +1,5 @@
+// Find the div you want to replace
+const divToReplace = document.querySelector(".product-title");
+
+// Replace the content of the div
+divToReplace.innerHTML = "New content goes here!";
