00ff4c5c0bdc9be2bbedb33179e18bd97dd2586b
The armors'll save us all <jl_beuzard@hotmail.com>
2024-06-01 04:33:17 +0200

    ! init

--- Horloge-test-cSharp/src/main.js
+++ Horloge-test-cSharp/src/main.js
@@ -2,17 +2,10 @@ const { invoke } = window.__TAURI__.tauri;
 
 let greetInputEl;
 let greetMsgEl;
-// Créez une nouvelle instance de l'objet Audio
-let audio = new Audio('assets/RihannaSOS.mp3');
-
-function playSong() {
-  audio.play();
-}
 
 async function greet() {
   // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
   greetMsgEl.textContent = await invoke("greet", { name: greetInputEl.value });
-  playSong();
 }
 
 window.addEventListener("DOMContentLoaded", () => {
