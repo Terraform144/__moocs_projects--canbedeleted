4d92e2faa8447f4f2593b5da0505feddf1ae51b2
The armors'll save us all. <jl_beuzard@hotmail.com>
2024-09-04 00:13:21 +0200

    !add

--- _cpp/WpfApp1/obj/Debug/net6.0-windows/WpfApp1.AssemblyInfo.cs
+++ _cpp/WpfApp1/obj/Debug/net6.0-windows/WpfApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WpfApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2bfbe484487af98e3c045968df0afa72dc7062b8")]
 [assembly: System.Reflection.AssemblyProductAttribute("WpfApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WpfApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
