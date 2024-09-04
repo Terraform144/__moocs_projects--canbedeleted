1740492226e8349997f587206784d18649aaed1a
The armors'll save us all. <jl_beuzard@hotmail.com>
2024-09-04 02:27:25 +0200

    del

--- _microsoft/ConsoleApp1_linq/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-
-int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 13, 65, 48 };
-
-var getTheNumbers = from number in numbers where number < 5 select number;
-
-foreach (var n in getTheNumbers) Console.WriteLine(n);
-
