15f924d6f120b8a2ceba48e7cdaa40a1764dfb20
terground <terground@gmail.com>
2025-05-26 19:16:07 +0200

    ! APP 3

---
+++ svelte/01-premier/src/App3.svelte
@@ -0,0 +1,12 @@
+<script>
+  import Fiche from './Components/Fiche.svelte';
+  let prenom = '';
+  let nom = '';
+  let age = '';
+</script>
+
+<input type="text" placeholder="prenom" id="prenom" bind:value={prenom}>
+<input type="text" placeholder="nom" id="nom" bind:value={nom}>
+<input type="text" placeholder="age" id="age" bind:value={age}>
+<Fiche {prenom} {nom} {age} />
+  
---
+++ svelte/01-premier/src/Components/Fiche.svelte
@@ -0,0 +1,19 @@
+<script>
+    export let prenom, nom, age;
+</script>
+<div>
+    <p>Nom : {prenom} {nom}</p>
+    <p>Age : {age}</p>
+</div>
+<style>
+    div {
+        border: 1px solid black;
+        padding: 10px;
+        margin: 10px;
+        background: lightgreen;
+        font-size: 2rem;
+    }
+    p {
+        margin: 5px 0;
+    }
+</style>
