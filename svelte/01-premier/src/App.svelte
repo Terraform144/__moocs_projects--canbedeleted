7b6e31a8cc92cecd7fa754a45027357cce98a647
terground <terground@gmail.com>
2025-05-26 17:48:35 +0200

    !svelte

---
+++ svelte/01-premier/src/App.svelte
@@ -0,0 +1,14 @@
+<script>
+  let src='https://bit.ly/3GOTubz';
+  let logo='Logo Svelte';
+  let taille=100;
+  let url='https://svelte.dev';
+  let lien='cliquez ici';
+  let compteur=0;
+  function incrementer() {
+    compteur += 1;
+  }
+</script>
+<p><button on:click={incrementer}>{compteur}</button></p>
+<p><a href={url}>{lien}</a></p>
+<img {src} alt={logo} width={taille}>
