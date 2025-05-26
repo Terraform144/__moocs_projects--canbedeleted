495e5ed0341a99d7a3c874b6edd21cbac11e615a
terground <terground@gmail.com>
2025-05-26 19:38:10 +0200

    ! @html

--- svelte/01-premier/src/App4.svelte
+++ svelte/01-premier/src/App4.svelte
@@ -1,6 +1,8 @@
 <script>
   import Compteur, {grandTotal} from "./Components/Compteur.svelte";
   let somme = 0;
+ // text brut et html
+  let txtHTML = 'this <strong>text</button>strong>';
 </script>
 <Compteur />
 <Compteur />
@@ -9,5 +11,12 @@
 <button on:click ={() => {
   somme = grandTotal();
   alert(`Le total est ${somme}`);
+
+ 
+ 
+
 }}>Afficher le total</button>
 <span>{somme}</span>
+<hr>
+{txtHTML}<br>
+{@html txtHTML}
