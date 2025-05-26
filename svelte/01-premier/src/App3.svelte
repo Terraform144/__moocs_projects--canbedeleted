943fe2b6c2079e5e7479541c64f3a7264ff6fc84
terground <terground@gmail.com>
2025-05-26 19:24:49 +0200

    ! json props databind

--- svelte/01-premier/src/App3.svelte
+++ svelte/01-premier/src/App3.svelte
@@ -1,12 +1,11 @@
 <script>
   import Fiche from './Components/Fiche.svelte';
-  let prenom = '';
-  let nom = '';
-  let age = '';
+  let data = {prenom : '', nom : '', age : ''};
+
 </script>
 
-<input type="text" placeholder="prenom" id="prenom" bind:value={prenom}>
-<input type="text" placeholder="nom" id="nom" bind:value={nom}>
-<input type="text" placeholder="age" id="age" bind:value={age}>
-<Fiche {prenom} {nom} {age} />
-  
+<input type="text" placeholder="prenom" id="prenom" bind:value={data.prenom}>
+<input type="text" placeholder="nom" id="nom" bind:value={data.nom}>
+<input type="text" placeholder="age" id="age" bind:value={data.age}>
+<Fiche {...data} />
+<Fiche prenom={data.prenom} nom={data.nom} age={data.age} />
