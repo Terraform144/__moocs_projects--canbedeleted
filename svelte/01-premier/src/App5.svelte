a00bb4432940da735b07e72e06ac350212d7dda0
terground <terground@gmail.com>
2025-05-26 19:54:15 +0200

    ! classes conditionnelles

---
+++ svelte/01-premier/src/App5.svelte
@@ -0,0 +1,15 @@
+<script>
+  let bool= true;
+  function toogle(){
+    bool = !bool;
+  }
+</script>
+<style>
+  .rouge {color: red;}
+  .noir {color: black;}
+</style>
+<button on:click={toogle}>Changer la couleur</button>
+<span class={bool ? 'rouge' : 'noir'}>Texte en couleur</span>
+
+<button on:click={toogle}>Changer la couleur</button>
+<p class:rouge={bool}>text simple</p>
