0ec5682d276937336af345b11ff815ce24b47c26
terground <terground@gmail.com>
2025-05-26 18:00:16 +0200

    mod data binding

---
+++ svelte/01-premier/src/App2.svelte
@@ -0,0 +1,13 @@
+<script>
+let image = 'https://bit.ly/3GOTubz';
+function change() {
+  image = 'https://bit.ly/3GOTubz';
+}
+let prenom = 'Jean';
+</script>
+<img src={image} on:click={change}>
+<hr>
+<label for="prenom">quel est votre prénom ?</label>
+<input type="text" id="prenom" bind:value={prenom}>
+<h2>Bonjour {prenom}</h2>
+  
